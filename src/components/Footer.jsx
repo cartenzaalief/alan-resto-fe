7a2712782219bb629536e0c5a52c436fad15adde
cartenzaalief <cartenzaalief@gmail.com>
2023-06-23 20:38:33 +0700

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 const Footer = () => {
     return (
-        <Box bgColor="gray.50" px={24} h="10vh">
+        <Box bgColor="#F8F8F8" px={24} h="10vh">
             <Flex justifyContent="center" alignItems="center" h="full">
                 <Text fontSize="sm" color="gray">Alan Resto © 2023 | Developed by Alan Creative</Text>
             </Flex>
